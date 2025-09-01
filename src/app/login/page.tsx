07c312cc47934c38c572a1271eb23f55f2fb70ba
Vladyslava Podzirei <vladapodzirey@gmail.com>
2025-09-01 14:51:46 +0300

    fix login component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,11 @@
 'use client'
+import { Suspense } from 'react'
 import LoginComponent from '@/components/login.component'
 
 export default function LoginPage() {
-	return <LoginComponent />
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<LoginComponent />
+		</Suspense>
+	)
 }
